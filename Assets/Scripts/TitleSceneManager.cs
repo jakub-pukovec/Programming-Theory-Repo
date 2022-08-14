db0296474cd328364aade86d1547f204d79c15c1
Jakub Pukovec <jakub.pukovec@gmail.com>
2022-08-15 01:47:05 +0200

    Starting game when start button is pressed, quitting game on quit button.

---
+++ Assets/Scripts/TitleSceneManager.cs
@@ -0,0 +1,22 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}

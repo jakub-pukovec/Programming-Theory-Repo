ce5afec632269d6e312fa7598e0a6033ef902054
Jakub Pukovec <jakub.pukovec@gmail.com>
2022-08-15 02:17:48 +0200

    Adding duplicated info panel for switch, to be refactored later

--- Assets/Scripts/SwithController.cs
+++ Assets/Scripts/SwithController.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class SwithController : MonoBehaviour
@@ -6,15 +7,27 @@ public class SwithController : MonoBehaviour
 
     private ISwitchable _switchableScript;
     private bool _isInTriggerZone;
+    private GameObject _itemInfo;
+    private TMP_Text _infoTitleText;
+    private TMP_Text _infoDescriptionText;
 
     private void Awake()
     {
+        _itemInfo = GameObject.Find("ItemInfo");
+        _infoTitleText = _itemInfo.transform.Find("TitleText").GetComponent<TMP_Text>();
+        _infoDescriptionText = _itemInfo.transform.Find("ScoreText").GetComponent<TMP_Text>();
+
         _switchableScript = _switchable.GetComponent<ISwitchable>();
         if (_switchableScript == null)
         {
             Debug.LogError("Switchable game object must implement ISwitchable interface.");
             return;
         }
+    }
+
+    private void Start()
+    {
+        _itemInfo.SetActive(false);
     }
 
     private void Update()
@@ -45,6 +58,27 @@ public class SwithController : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             _isInTriggerZone = false;
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        _itemInfo.transform.position = transform.position + Vector3.up * .35f;
+        _itemInfo.SetActive(true);
+        _infoTitleText.text = "Platform Switch";
+        _infoDescriptionText.text = "Hit Use button [E] when touching the switch to switch platform on / off.";
+    }
+
+    private void OnMouseExit()
+    {
+        _itemInfo.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        if (_itemInfo != null)
+        {
+            _itemInfo.SetActive(false);
         }
     }
 }
